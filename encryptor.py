3c5dcd0c628648c0c8eadaf81fe7b241eaf1c385
Sahma61 <sahmaanwar61@gmail.com>
2024-05-23 13:24:43 +0530

    added lintings and docstrings

--- encryptor.py
+++ encryptor.py
@@ -1,36 +1,60 @@
-import sys
+#!/usr/bin/python3
+"""
+Encryptor Module.
 
-from Cryptodome.Cipher import AES
+encrypts and decrypts plaintext
+using AES.
+a user supplied password is used as the key
+"""
+import sys
+import getpass
+
 from io import BytesIO
 from base64 import b64encode, b64decode
+from Cryptodome.Cipher import AES
+
 
 def encrypt(password, text, create_file=False):
-    for i in range(16 - len(password) % 16):
+    """
+    Encryption Function.
+
+    encrypts plaintext using AES.
+    a user supplied password is used as the key
+    """
+    for _ in range(16 - len(password) % 16):
         password = password + "0"
-    
+
     key = password.encode()
     cipher = AES.new(key, AES.MODE_EAX)
     ciphertext, tag = cipher.encrypt_and_digest(bytes(text.encode()))
 
     if create_file:
-        file_out = open("encrypted.bin", "wb")
-        [ file_out.write(x) for x in (cipher.nonce, tag, ciphertext) ]
-        file_out.close()
+        with open("encrypted.bin", "wb") as file_out:
+            for info in (cipher.nonce, tag, ciphertext):
+                file_out.write(info)
 
-    #Write to string as file
+    # Write to string as file
     file_out = BytesIO()
-    [ file_out.write(x) for x in (cipher.nonce, tag, ciphertext) ]
+    for info in (cipher.nonce, tag, ciphertext):
+        file_out.write(info)
 
     return b64encode(file_out.getvalue()).decode()
 
+
 def decrypt(password, text):
-    for i in range(16 - len(password) % 16):
+    """
+    Decryption Function.
+
+    decrypts plaintext using AES.
+    a user supplied password is used as the key
+    """
+    for _ in range(16 - len(password) % 16):
         password = password + "0"
 
     file_in = BytesIO(bytes(b64decode(text.encode())))
-    
+
     key = password.encode()
-    nonce, tag, ciphertext = [ file_in.read(x) for x in (16, 16, -1) ]
+    nonce, tag, ciphertext = [file_in.read(x) for x in (16, 16, -1)]
     cipher = AES.new(key, AES.MODE_EAX, nonce)
     try:
         data = cipher.decrypt_and_verify(ciphertext, tag)
@@ -39,15 +63,23 @@ def decrypt(password, text):
         sys.exit(0)
     return data.decode()
 
+
 def get_password():
-    while(True):
-        password1 = input("Enter the password:")
-        password2 = input("Enter the password to confirm:")
+    """
+    Get Password  Function.
+
+    accepts a passowrd via user input and
+    confirms it
+    """
+    while True:
+        password1 = getpass.getpass("Enter the password:")
+        password2 = getpass.getpass("Enter the password to confirm:")
         if password1 != password2:
             print("Passwords don't match")
             continue
         break
     return password1
+
 
 if __name__ == "__main__":
     option = input("Enter encrypt/decrypt Option:")
@@ -55,11 +87,11 @@ if __name__ == "__main__":
         print("Option not supported!")
         sys.exit(0)
 
-    text = input("Enter Text:")
-    
-    password = get_password()
+    txt = input("Enter Text:")
+
+    passwrd = get_password()
 
     if option == "encrypt":
-        print(encrypt(password, text))
+        print(encrypt(passwrd, txt))
     else:
-        print(decrypt(password, text))
+        print(decrypt(passwrd, txt))
